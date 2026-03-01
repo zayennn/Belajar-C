cbd087e6214e437dd8fc4c85792584af1f251101
zayennn <zaayeennn@gmail.com>
2026-03-02 06:52:22 +0700

    upload

---
+++ 01. Fondasi C ( Basic Structure + Output )/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h> // ? ini namanya preprocessor directive, tandanya # di depan
+// * artinya: sebelum program di-compile, lakukan sesuatu dulu
+
+// ? stdio.h adalah library bawaan c
+/* isinya fungsi :
+* • printf
+* • scanf
+* • dll 
+todo tanpa ini ( stdio.h ) bakalan error karena compiler gatau itu apa
+*/
+
+
+int main() { // ? ini adalah fungsi utama, artinya program akan mulai dari sini
+    // ? main pintu masuk utama program
+    // ? int adalah tipe data, artinya variabel ini akan ngembalikan angka (integer)
+    // ? kenapa harus int ? karena sistem operasi nunggu angka balikan dari program , biasa nya :
+    // * 0 artinya sukses, selain itu artinya error
+
+    printf("Hello, World!\n");
+    return 0;
+}
