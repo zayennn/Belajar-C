29d427f76f6a747ac306fffbd305d604ab5f4e64
zayennn <zaayeennn@gmail.com>
2026-03-02 08:36:30 +0700

    update

--- 03. Input, If Else, Dan Logika/2. If Else/main.c
+++ 03. Input, If Else, Dan Logika/2. If Else/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    
+    int nilai;
+    printf("masukan nilai ujian : ");
+    scanf("%d", &nilai);
+
+    if ( nilai >= 75 ) {
+        printf("lulus");
+    } else {
+        printf("tidak lulus");
+    }
+
+    return 0;
 }
