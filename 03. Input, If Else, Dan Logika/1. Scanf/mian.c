b4ae85004b9d7e94de7a277140456b4e86e3ca54
zayennn <zaayeennn@gmail.com>
2026-03-02 08:32:28 +0700

    update

--- 03. Input, If Else, Dan Logika/1. Scanf/mian.c
+++ 03. Input, If Else, Dan Logika/1. Scanf/mian.c
@@ -6,6 +6,14 @@ int main() {
     printf("masukan umur kamu : ");
     scanf("%d", &umur);
 
+    /*
+        ? scanf("%d", &umur);
+        * %d → tipe data integer yang mau dibaca
+        * &umur → alamat memory variabel umur
+            * • scanner butuh alamat supaya bisa masukin nilai ke variabel
+            * • tanpa & → error
+    */
+
     printf("umur kamu : %d", umur);
 
     return 0;
