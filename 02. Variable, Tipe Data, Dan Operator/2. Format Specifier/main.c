5e6a681349fd72a0fb0640a24adf8485bbb6fdb2
zayennn <zaayeennn@gmail.com>
2026-03-02 07:38:22 +0700

    update

--- 02. Variable, Tipe Data, Dan Operator/2. Format Specifier/main.c
+++ 02. Variable, Tipe Data, Dan Operator/2. Format Specifier/main.c
@@ -4,11 +4,6 @@ int main() {
     int umur = 19;
 
     printf("umur kamu : %d\n", umur); // ? %d = format specifier untuk menampilkan nilai integer
-    printf("umur kamu : %i\n", umur); // ? %i = format specifier untuk menampilkan nilai integer ( sama dengan %d )
-    printf("umur kamu : %u\n", umur); // ? %u = format specifier untuk menampilkan nilai unsigned integer ( hanya untuk bilangan positif )
-    printf("umur kamu : %o\n", umur); // ? %o = format specifier untuk menampilkan nilai octal ( basis 8 )
-    printf("umur kamu : %x\n", umur); // ? %x = format specifier untuk menampilkan nilai hexadecimal ( basis 16 ) dengan huruf kecil
-    printf("umur kamu : %X\n", umur); // ? %X = format specifier untuk menampilkan nilai hexadecimal ( basis 16 ) dengan huruf besar
 
     return 0;
 }
