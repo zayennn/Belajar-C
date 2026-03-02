cf7f55e9e8c1da90e5fbb3147bc6b5c438b0bb27
zayennn <zaayeennn@gmail.com>
2026-03-02 07:33:58 +0700

    update

--- 02. Variable, Tipe Data, Dan Operator/main.c
+++ 02. Variable, Tipe Data, Dan Operator/main.c
@@ -6,6 +6,17 @@ int main()
     float tinggi = 175.5;
     char grade = 'A';
 
+    // ? int = untuk menyimpan bilangan bulat
+    // ? float = untuk menyimpan bilangan desimal
+    // ? char = untuk menyimpan karakter ( 1 karakter , harus menggunakan tanda petik tunggal '' )
+
+    // ? int → tipe data
+    // ? umur → nama variabel
+    // ? = → operator penugasan / assignment operator
+    // ? 20 → nilai yang akan disimpan dalam variabel umur
+    // ? ; → akhir statement
+    // * artinya bikin variable dengan tipe data int yang nama nya umur dan isi/value nya adalah 20
+
     printf("umur kamu : %d\n", umur);
     printf("tinggi kamu : %.1f\n", tinggi);
     printf("grade : %c\n", grade);
