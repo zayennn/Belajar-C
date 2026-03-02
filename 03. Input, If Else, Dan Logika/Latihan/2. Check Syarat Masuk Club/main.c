3b5fc76bd586ddac769b42c9bfeb896128661113
zayennn <zaayeennn@gmail.com>
2026-03-02 08:52:15 +0700

    update

---
+++ 03. Input, If Else, Dan Logika/Latihan/2. Check Syarat Masuk Club/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int umur;
+    float tinggi;
+
+    printf("masukan umur anda : ");
+    scanf("%d", &umur);
+    printf("masukan tinggi anda : ");
+    scanf("%.1f", tinggi);
+
+    if ( umur >= 18 && tinggi >= 160 ) {
+        printf("memenuhi syarat");
+    } else {
+        printf("tidak memenuhi syarat");
+    }
+}
