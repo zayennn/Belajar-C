7dbda2e3446f293f884ee039a19a2e177f2267f2
zayennn <zaayeennn@gmail.com>
2026-03-02 07:29:12 +0700

    update

--- 02. Variable, Tipe Data, Dan Operator/main.c
+++ 02. Variable, Tipe Data, Dan Operator/main.c
@@ -4,9 +4,11 @@ int main()
 {
     int umur = 20;
     float tinggi = 175.5;
+    char grade = 'A';
 
     printf("umur kamu : %d\n", umur);
     printf("tinggi kamu : %.1f\n", tinggi);
+    printf("grade : %c\n", grade);
 
     return 0;
 }
