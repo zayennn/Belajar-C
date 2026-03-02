bf8b24584691ec9385d1338c1e465c139aae6ea1
zayennn <zaayeennn@gmail.com>
2026-03-02 07:41:36 +0700

    update

--- 02. Variable, Tipe Data, Dan Operator/3. Operator Aritmatika/mian.c
+++ 02. Variable, Tipe Data, Dan Operator/3. Operator Aritmatika/mian.c
@@ -7,4 +7,5 @@ int main() {
     printf("a + b = %d\n", a + b); // Penjumlahan
     printf("a - b = %d\n", a - b); // Pengurangan
     printf("a * b = %d\n", a * b); // Perkalian
+    printf("a / b = %d\n", a / b); // Pembagian
 }
