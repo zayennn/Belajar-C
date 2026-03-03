5413f703da13b97ddd8e4b5bca8a989e8df34b09
zayennn <zaayeennn@gmail.com>
2026-03-04 00:08:03 +0700

    update

--- 04. Loop ( For, While , Do While )/4. Nested Loop/main.c
+++ 04. Loop ( For, While , Do While )/4. Nested Loop/main.c
@@ -2,7 +2,7 @@
 
 int main() {
     for (int i = 1; i <= 3; i++) {
-        for (int j = 1; j <= i; j++) {
+        for (int j = 1; j <= 3; j++) {
             printf("%d", j);
         }
         printf("\n");
