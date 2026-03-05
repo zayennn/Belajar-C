dfbd1b0f7621e8eadb0fdaf52135f9ef6b9696ac
zayennn <zaayeennn@gmail.com>
2026-03-06 06:29:59 +0700

    update

--- 05. Function/1. Apa Itu Function/main.c
+++ 05. Function/1. Apa Itu Function/main.c
@@ -1,2 +1,16 @@
 // apa itu function
 // function itu blok kode yang bisa dipanggil berulang
+
+// contoh :
+#include <stdio.h>
+
+void halo() {
+    printf("halo\n");
+}
+
+int main() {
+    halo();
+    halo();
+
+    return 0;
+}
