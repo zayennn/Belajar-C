f494a7a58da3add10be3519acdf78860bd168ca0
zayennn <zaayeennn@gmail.com>
2026-03-06 06:38:52 +0700

    update

--- 05. Function/2. Function Dengan Parameter/main.c
+++ 05. Function/2. Function Dengan Parameter/main.c
@@ -16,3 +16,15 @@ int main() {
 // * fulan = f u l a n \0
 // * \0 adalah null terminator (penanda akhir string)
 
+// * bentuk asli :
+// ? char nama[] = "fulana";
+// * artinya :
+/*
+    nama[0] = 'f'
+    nama[1] = 'u'
+    nama[2] = 'l'
+    nama[3] = 'a'
+    nama[4] = 'n'
+    nama[5] = 'a'
+    nama[6] = '\0'
+*/
