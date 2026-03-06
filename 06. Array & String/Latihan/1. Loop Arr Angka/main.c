c94e1753f1cfbd7d60cf498f93e115a9aa34406b
zayennn <zaayeennn@gmail.com>
2026-03-06 09:27:11 +0700

    update

--- 06. Array & String/Latihan/1. Loop Arr Angka/main.c
+++ 06. Array & String/Latihan/1. Loop Arr Angka/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int angka[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int angka[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+
+    for ( int i = 0; i < angka[i]; i++ ) {
+        printf("%d", i);
+    }
+
+    return 0;
 }
