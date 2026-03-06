481075eb85e38123146e37a8c47d0f4772074f95
zayennn <zaayeennn@gmail.com>
2026-03-06 08:09:22 +0700

    update

--- 05. Function/Latihan/Mini Project/main.c
+++ 05. Function/Latihan/Mini Project/main.c
@@ -4,3 +4,38 @@
 void tampilkanIntro() {
     printf("masukan tebakan mu (1-10) : ");
 }
+
+int generateAngka() {
+    int com = rand() % 10 + 1;
+    return com;
+}
+
+void jalankanGame() {
+    int tebakan_user;
+
+    printf("tebak angka 1-10 : ");
+    scanf("%d", &tebakan_user);
+
+    int kesempatan_user = 5;
+
+    while (5 > 0) {
+        if (tebakan_user != generateAngka()) {
+            if (tebakan_user > generateAngka()) {
+                printf("tebakan mu salah, angka mu terlalu besar...\n");
+                printf("sisa kesempatan mu : %d\n", kesempatan_user);
+                printf("tebak angka 1-10 : ");
+                scanf("%d", &tebakan_user);
+                kesempatan_user--;
+            } else if (tebakan_user < generateAngka()) {
+                printf("tebakan mu salah, angka mu terlalu kecil...\n");
+                printf("sisa kesempatan mu : %d\n", kesempatan_user);
+                printf("tebak angka 1-10 : ");
+                scanf("%d", &tebakan_user);
+                kesempatan_user--;
+            } 
+        } else {
+            printf("tebakan mu benar");
+            break;
+        }
+    }
+}
