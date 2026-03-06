15332bbe6c7353e4488b00826a17c0bd8e14b740
zayennn <zaayeennn@gmail.com>
2026-03-06 11:04:34 +0700

    update

--- 06. Array & String/Mini Project/main.c
+++ 06. Array & String/Mini Project/main.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main() {
-
-    int jumlah_mahasiswa[3];
-    char *nama;
-    int nilai;
+    char nama[3][50];
+    int nilai[3];
 
     for ( int i = 0; i < 3; i++ ) {
         printf("masukan nama mahasiswa ke - %d : ", i + 1);
-        scanf("%s", nama);
+        scanf("%s", &nama);
         printf("masukan nilai mahasiswa ke - %d : ", i + 1);
-        scanf("%d", nilai);
+        scanf("%d", &nilai);
     }
 
-    
+    for ( int i = 0; i < 3; i++ ) {
+        printf("nama mahasiswa ke %d : %s\n", (i + 1), nama[i]);
+        printf("nilai nya : %d\n", nilai[i]);
+        printf("\n");
+    }
 
     return 0;
 }
