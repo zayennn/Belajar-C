c0f80ae529a6c79ef6409495acf64b7f1a82c78e
zayennn <zaayeennn@gmail.com>
2026-03-06 09:45:20 +0700

    update

--- 06. Array & String/Latihan/2. User Input/main.c
+++ 06. Array & String/Latihan/2. User Input/main.c
@@ -3,6 +3,7 @@
 int main() {
     int n[5];
     int total = 0;
+    int rata_rata;
 
     for ( int i = 0; i < 5; i++ ) {
         printf("nilai ke - %d : ", i + 1);
@@ -11,10 +12,13 @@ int main() {
 
     for ( int i = 0; i < n[i]; i++ ) {
         total += n[i];
+
+        rata_rata = total / i;
         // printf("%d\n", n[i]);
     }
 
     printf("%d", total);
+    printf("%d", rata_rata);
 
     return 0;
 }
