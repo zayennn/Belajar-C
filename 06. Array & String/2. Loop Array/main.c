cbf0018a9bca7fe74858168aab34cd876ed505cd
zayennn <zaayeennn@gmail.com>
2026-03-06 08:39:22 +0700

    update

--- 06. Array & String/2. Loop Array/main.c
+++ 06. Array & String/2. Loop Array/main.c
@@ -2,8 +2,9 @@
 
 int main() {
     int angka[5] = {10, 20, 30, 40, 50};
+    int length = sizeof(angka) / sizeof(angka[0]);
 
-    for ( int i = 0; i < 5; i++ ) {
+    for ( int i = 0; length < 5; i++ ) {
         printf("%d\n", angka[i]);
     }
 
