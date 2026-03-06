18a820d1ca42d3adc5ffcd7b4e0869015b8a31fb
zayennn <zaayeennn@gmail.com>
2026-03-06 09:39:33 +0700

    update

--- 06. Array & String/Latihan/2. User Input/main.c
+++ 06. Array & String/Latihan/2. User Input/main.c
@@ -4,7 +4,12 @@ int main() {
     int n[5];
 
     for ( int i = 0; i < 5; i++ ) {
-        
+        printf("nilai ke - %d : ", i + 1);
+        scanf("%d", &n);
+    }
+
+    for ( int i = 0; i < 5; i++ ) {
+        printf("%d\n", n[i]);
     }
 
     return 0;
