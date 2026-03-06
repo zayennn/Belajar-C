89c134d8c5bbaea4eb2d0ad44375546a99c6952e
zayennn <zaayeennn@gmail.com>
2026-03-06 08:27:04 +0700

    update

---
+++ 06. Array & String/1. Apa Itu Array/main.c
@@ -0,0 +1,12 @@
+// * array adalah kumpulan data dalam 1 variable, contoh :
+// ? int nilai[5];
+/*
+    * artinya :
+    nilai[0]
+    nilai[1]
+    nilai[2]
+    nilai[3]
+    nilai[4]
+
+    ! index selalu dimulai dari 0
+*/
