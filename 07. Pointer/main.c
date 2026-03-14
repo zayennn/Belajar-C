7fde439e01f8faa401767675a17826759c249d31
zayennn <zaayeennn@gmail.com>
2026-03-14 12:59:57 +0700

    update

--- 07. Pointer/main.c
+++ 07. Pointer/main.c
@@ -1,11 +1,15 @@
 /*
     apa itu pointer ?
+
     komputer menyimpan data di memory (RAM)
     bayangkan RAM itu seperti deretan rumah, contoh :
     Alamat      Isi
     1000        10
     1004        20
     1008        30
+
+    variable bisa :
+    ? int a = 10;
 */
 
 #include <stdio.h>
