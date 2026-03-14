5dc4a31501320c8d6710597526de8453334b8976
zayennn <zaayeennn@gmail.com>
2026-03-14 12:20:56 +0700

    update

--- 07. Pointer/main.c
+++ 07. Pointer/main.c
@@ -2,4 +2,8 @@
     apa itu pointer ?
     komputer menyimpan data di memory (RAM)
     bayangkan RAM itu seperti deretan rumah, contoh :
+    Alamat      Isi
+    1000        10
+    1004        20
+    1008        30
 */
