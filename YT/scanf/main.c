72efd6133a29df24f2e7932f1003418e9f73d22e
zayennn <zaayeennn@gmail.com>
2026-03-22 07:46:16 +0700

    update

--- YT/scanf/main.c
+++ YT/scanf/main.c
@@ -2,8 +2,8 @@
 
 int main() {
     int x; // * untuk tipe data number/integer menggunakan %d
-    char y; // * untuk tipe data char menggunakan %c
-    float z; // * untuk tipe data float menggunakan %f
+    char y; // * untuk tipe data char menggunakan %c, scan char itu harus ada spasi di awal %c nya
+    float z; // * untuk tipe data float menggunakan %f , 1-4 pembulatan ke bawah , 5-9 pembulatan ke atas
 
     printf("masukan nilai x : ");
     scanf("%d", &x);
